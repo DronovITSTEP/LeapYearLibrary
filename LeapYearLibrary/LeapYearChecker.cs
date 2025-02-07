00970c1f8705c1a2f111305934d9140fd38d2eaa
daniil <skrimexgame12508@mail.ru>
2025-02-07 19:00:35 +0300

    Добавьте файлы проекта.

---
+++ LeapYearLibrary/LeapYearChecker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYearLibrary
+{
+    public class LeapYearChecker
+    {
+        public static bool IsLeapYear(int year)
+        {
+            return (year % 4 == 0 && year % 100 != 0) ||
+                (year % 400 == 0);
+        }
+    }
+}
---
+++ LeapYearTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeapYearLibrary;
+
+namespace LeapYearTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год");
+            int year = int.Parse(Console.ReadLine());
+
+            bool isLeapYear = LeapYearChecker
+                .IsLeapYear(year);
+
+            if (isLeapYear)
+                Console.WriteLine("год високосный");
+            else
+                Console.WriteLine("год не високосный");
+        }
+    }
+}
